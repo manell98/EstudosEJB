e8ede593b5aa292c0da089c9a4aa90bb52c2e3b1
Emanuell Santos <dfmanu06@gmail.com>
2019-09-04 20:48:20 -0300

    Integracao do JPA com Pool e DataSource

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -3,27 +3,27 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager em;
 
 	public void salva(Autor autor) {
-		banco.save(autor);
+		this.em.persist(autor);
 	}
 	
 	public List<Autor> todosAutores() {
-		return banco.listaAutores();
+		return this.em.createQuery("select a from Autor a", Autor.class).getResultList();
 	}
 
 	public Autor buscaPelaId(Integer autorId) {
-		Autor autor = this.banco.buscaPelaId(autorId);
-		return autor;
+		return this.em.find(Autor.class, autorId);
 	}
 	
 }
