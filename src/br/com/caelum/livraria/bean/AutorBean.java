b1ec026964af300e911e67268308680664a7319e
Emanuell Santos <dfmanu06@gmail.com>
2019-09-04 16:32:59 -0300

    Ciclo de vida dos Sessions Beans

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -2,16 +2,21 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
-import javax.enterprise.inject.Model;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import br.com.caelum.livraria.dao.AutorDao;
 import br.com.caelum.livraria.modelo.Autor;
 
-@Model
+@Named
+@RequestScoped
 public class AutorBean {
 	
 	private Autor autor = new Autor();
-	private AutorDao dao = new AutorDao();
+	
+	@Inject
+	private AutorDao dao;
 	
 	public Autor getAutor() {
 		return autor;
